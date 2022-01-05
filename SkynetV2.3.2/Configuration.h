e82c307edd3b2fba7601b8e1b666d7dfa42d2104
FPatV <31507327+patrick131184@users.noreply.github.com>
2022-01-05 23:30:51 +0100

    Update Configuration.h
    Extruder Calibration to 100mm

--- SkynetV2.3.2/Configuration.h
+++ SkynetV2.3.2/Configuration.h
@@ -492,7 +492,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,  100, 400, 94.27}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,  100, 400, 95.1}
 
 /**
  * Default Max Feed Rate (mm/s)
